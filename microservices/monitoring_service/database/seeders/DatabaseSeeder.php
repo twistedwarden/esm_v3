1e611d5edb3a0c67273ae9a9b592e309d7e6735c
twistedwarden <mahusaycchristian@gmail.com>
2026-01-27 18:03:17 +0800

    rm monitoring seeders

--- microservices/monitoring_service/database/seeders/DatabaseSeeder.php
+++ microservices/monitoring_service/database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            AnalyticsSeeder::class,
-        ]);
+        // $this->call([
+        //     AnalyticsSeeder::class,
+        // ]);
     }
 }
